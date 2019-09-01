2f32560cd490cb7c22d547333834d2a9399c7fad
Raphael Santos <raphaelmacsa@gmail.com>
2019-09-01 06:57:16 -0300

    Add express api application and frontend

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const smsRoute = require('./lib/api/routes/sms');
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/sms', smsRoute);
+app.use(express.static('lib/public'));
+
+const server = app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`Server running on port ${server.address().port}`);
+});
